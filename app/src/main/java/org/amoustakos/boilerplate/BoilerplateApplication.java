fa8f32d55c03a20d0b558b8b12346561708581d8
antonis179 <antonis179@gmail.com>
2017-09-02 01:17:56 +0300

    Init commit

---
+++ app/src/main/java/org/amoustakos/boilerplate/BoilerplateApplication.java
@@ -0,0 +1,12 @@
+package org.amoustakos.boilerplate;
+
+import android.app.Application;
+
+/**
+ * Created by antonis on 02/09/2017.
+ */
+
+public class BoilerplateApplication extends Application {
+
+
+}
