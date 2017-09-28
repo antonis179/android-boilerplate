7c33948deb70e297e805f2abdd52c99411ec3e75
antonis179 <antonis179@gmail.com>
2017-09-28 22:31:40 +0300

    - Added reflection utility class with code to find classes in a specific file or package

---
+++ app/src/main/java/org/amoustakos/boilerplate/examples/ui/activities/MainActivity.java
@@ -0,0 +1,72 @@
+package org.amoustakos.boilerplate.examples.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.RecyclerView;
+
+import org.amoustakos.boilerplate.R;
+import org.amoustakos.boilerplate.ui.activities.base.BaseActivity;
+import org.amoustakos.boilerplate.util.ReflectionUtil;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Activity that lists all the activities in the same package and creates <br />
+ * a {@link RecyclerView} that starts each one on click.
+ */
+public class MainActivity extends BaseActivity {
+
+	private String basePackage;
+
+
+	/*
+	 * Views
+	 */
+
+	@BindView(R.id.rv_activity_pool)
+	RecyclerView exampleRecycler;
+
+
+	/*
+	 * Overrides
+	 */
+
+	@Override
+	@LayoutRes protected int layoutId() {
+		return R.layout.activity_main;
+	}
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ButterKnife.bind(this);
+
+
+		basePackage = this.getPackageName();
+	}
+
+
+
+	/*
+	 * UI
+	 */
+
+
+
+
+	/*
+	 * Reflection
+	 */
+
+	private List<Class<BaseActivity>> getActivities() {
+		return ReflectionUtil.listClasses(basePackage);
+	}
+
+
+
+
+}
---
+++ app/src/main/java/org/amoustakos/boilerplate/util/ReflectionUtil.java
@@ -0,0 +1,88 @@
+package org.amoustakos.boilerplate.util;
+
+import android.support.annotation.Nullable;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import timber.log.Timber;
+
+
+@SuppressWarnings("unchecked")
+public class ReflectionUtil {
+
+	private static final char PKG_SEPARATOR = '.';
+	private static final char DIR_SEPARATOR = '/';
+	private static final String CLASS_FILE_SUFFIX = ".class";
+	private static final String BAD_PACKAGE_ERROR = "Unable to get resources from path '%s'. Are you sure the package '%s' exists?";
+
+
+
+	/* =============================================================================================
+	 *                                     Class Reflections
+	 * ===========================================================================================*/
+
+	/**
+	 * Scans the provided package for the classes that match the infered type.
+	 */
+	public static <T> List<Class<T>> listClasses(String basePackage) {
+		String scannedPath = basePackage.replace(PKG_SEPARATOR, DIR_SEPARATOR);
+		URL scannedUrl = Thread.currentThread().getContextClassLoader().getResource(scannedPath);
+		if (scannedUrl == null)
+			throw new IllegalArgumentException(String.format(BAD_PACKAGE_ERROR, scannedPath, basePackage));
+
+
+		File scannedDir = new File(scannedUrl.getFile());
+		List<Class<T>> classes = new ArrayList<>();
+		for (File file : scannedDir.listFiles()) {
+			try {
+				classes.addAll(listClasses(file, basePackage));
+			} catch(ClassCastException ce) {
+				Timber.d(ce);
+			}
+		}
+		return classes;
+	}
+
+	/**
+	 * Scans a {@link File} for the classes that match the inferred type. <br />
+	 * If the file is under another package you will need to provide the parent package as well (null otherwise).
+	 */
+	public static <T> List<Class<T>> listClasses(File file, @Nullable String basePackage) {
+		List<Class<T>> classes = new ArrayList<>();
+		String resource;
+
+		if (basePackage != null)
+			resource = basePackage + PKG_SEPARATOR + file.getName();
+		else
+			resource = file.getName();
+
+		if (file.isDirectory())
+			for (File child : file.listFiles())
+				classes.addAll(listClasses(child, resource));
+
+		else if (resource.endsWith(CLASS_FILE_SUFFIX)) {
+			int endIndex = resource.length() - CLASS_FILE_SUFFIX.length();
+			String className = resource.substring(0, endIndex);
+			try {
+				Class<T> item = (Class<T>) Class.forName(className);
+				classes.add(item);
+			} catch (ClassNotFoundException | ClassCastException ce) {
+				Timber.d(ce);
+			}
+		}
+		return classes;
+	}
+
+
+
+
+
+
+
+
+
+
+}
