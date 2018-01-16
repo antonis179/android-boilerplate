ad404faef2abfe5262d18a4d2b72a87e22e40bb0
antonis179 <antonis179@gmail.com>
2018-01-16 19:17:02 +0200

    Set default NetEvent error state to true

--- models/src/main/java/org/amoustakos/models/network/NetEvent.kt
+++ models/src/main/java/org/amoustakos/models/network/NetEvent.kt
@@ -3,6 +3,6 @@ package org.amoustakos.models.network
 
 data class NetEvent<out T>(
         val error: Int? = null,
-        val hasError: Boolean = false,
+        val hasError: Boolean = true,
         val errorMessage: String,
         val item: T? = null)
