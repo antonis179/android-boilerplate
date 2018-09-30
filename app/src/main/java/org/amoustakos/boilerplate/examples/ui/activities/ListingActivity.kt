0566c0ab4d3e27c502954a15fcb83a5c48c8588d
antonis179 <antonis179@gmail.com>
2018-09-30 15:49:11 +0300

    Removed incomplete google assistant integration

--- app/src/main/java/org/amoustakos/boilerplate/examples/ui/activities/ListingActivity.kt
+++ app/src/main/java/org/amoustakos/boilerplate/examples/ui/activities/ListingActivity.kt
@@ -1,7 +1,5 @@
 package org.amoustakos.boilerplate.examples.ui.activities
 
-import android.app.SearchManager
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.annotation.LayoutRes
@@ -23,8 +21,6 @@ import java.util.*
  * Activity that lists all the activities in the same package and creates <br></br>
  * a [RecyclerView] that starts each one on click.
  */
-//adb shell am start -a com.google.android.gms.actions.SEARCH_ACTION -e query flights org.amoustakos.boilerplate
-//https://developers.google.com/voice-actions/system/#step_3_update_your_app_completion_status
 class ListingActivity : BaseActivity(), ActivityListingView {
 
 	private var isDoubleBackToExitPressedOnce = false
@@ -58,19 +54,12 @@ class ListingActivity : BaseActivity(), ActivityListingView {
 
 		setupRecycler()
 		presenter!!.load()
-
-		handleVoiceSearch(intent)
 	}
 
 
 	override fun onDestroy() {
 		super.onDestroy()
 		presenter?.unsubscribeFromLifecycle(lifecycle)
-	}
-
-	override fun onNewIntent(intent: Intent) {
-		super.onNewIntent(intent)
-		handleVoiceSearch(intent)
 	}
 
 	override fun onBackPressed() {
@@ -81,20 +70,6 @@ class ListingActivity : BaseActivity(), ActivityListingView {
 		isDoubleBackToExitPressedOnce = true
 		Toast.makeText(this, getString(R.string.toast_back_to_exit), Toast.LENGTH_SHORT).show()
 		Handler().postDelayed({ isDoubleBackToExitPressedOnce = false }, 2000)
-	}
-
-	// =========================================================================================
-	// Voice
-	// =========================================================================================
-
-	private fun handleVoiceSearch(intent: Intent?) {
-		if (intent != null && ACTION_VOICE_SEARCH == intent.action) {
-			val query = intent.getStringExtra(SearchManager.QUERY)
-			//TODO
-			Timber.i(intent.action)
-			Timber.i(intent.dataString)
-			Timber.i(query)
-		}
 	}
 
 	// =========================================================================================
@@ -119,9 +94,5 @@ class ListingActivity : BaseActivity(), ActivityListingView {
 		if (adapter == null)
 			adapter = ActivityListingAdapter(ArrayList())
 		rv_activity_pool.adapter = adapter
-	}
-
-	companion object {
-		private const val ACTION_VOICE_SEARCH = "com.google.android.gms.actions.SEARCH_ACTION"
 	}
 }
